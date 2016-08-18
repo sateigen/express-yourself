5380741ccb06b53903db3dc5dcee7477bcf338da
sateigen <shannon.teigen@gmail.com>
2016-08-17 23:03:24 -0400

    Function working to test emails in block of text.

--- textminer/extractor.py
+++ textminer/extractor.py
@@ -3,3 +3,7 @@ import re
 
 def phone_numbers(text):
     return re.findall(r"\(?[0-9]{3}\)?\.?\s?-?[0-9]{3}\-?\.?[0-9]{4}", text)
+
+
+def emails(text):
+    return re.findall(r"[a-z.]+@[a-z]+\.[a-z]{2,4}", text)

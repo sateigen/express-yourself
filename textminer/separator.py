d2043c6d19f294135404a26e6bdb3ca73159e39a
sateigen <shannon.teigen@gmail.com>
2016-08-17 21:36:12 -0400

    Function working to test words.

--- textminer/separator.py
+++ textminer/separator.py
@@ -1,13 +1,13 @@
 import re
 
 
-#  def words(string, count=None):
-#    string = string.split()
-#    if count:
-#        return count == len(string)
-#    for item in string:
-#        return word(item)
-
+def words(string):
+    string = string.split()
+    results = [word for word in string if re.search(r"^\w*\-?[a-z]+$", word)]
+    if results != []:
+        return results
+    else:
+        return None
 
 #def phone_number(num_string):
 #    return re.search(r"(\(?[0-9]{3}\))?\.?\s?[0-9]{3}\-?\.?[0-9]{4}", num_string)
@@ -21,29 +21,29 @@ import re
 #    return re.search(r"^\d{5}(\-?\d{4})?$", num_string)
 
 
-def date(date):
-    date_dict = {}
-    try:
-        groups = re.match(r'(\d{4})[/-](\d{0,2})[/-](\d{0,2})|(\d{0,2})[/-](\d{0,2})[/-](\d{4})', date).groups()
-        if groups[0]:
-            date_dict["year"] = int(groups[0])
-            date_dict["month"] = int(groups[1])
-            date_dict["day"] = int(groups[2])
-        else:
-            date_dict["month"] = int(groups[3])
-            date_dict["day"] = int(groups[4])
-            date_dict["year"] = int(groups[5])
-        return date_dict
-    except:
-        return None   
+#def date(date):
+#    date_dict = {}
+#    try:
+#        groups = re.match(r'(\d{4})[/-](\d{0,2})[/-](\d{0,2})|(\d{0,2})[/-](\d{0,2})[/-](\d{4})', date).groups()
+#        if groups[0]:
+#            date_dict["year"] = int(groups[0])
+#            date_dict["month"] = int(groups[1])
+#            date_dict["day"] = int(groups[2])
+#        else:
+#            date_dict["month"] = int(groups[3])
+#            date_dict["day"] = int(groups[4])
+#            date_dict["year"] = int(groups[5])
+#        return date_dict
+#    except:
+#        return None
 
 
 
 #if re.search(r"^\d{1,4}[/-]\d{1,2}[/-]\d{1,4}", string)
-    
-#possible_emails = ["clinton", "clinton@dreisbach.us", "beanguy@example.org", 
+
+#possible_emails = ["clinton", "clinton@dreisbach.us", "beanguy@example.org",
  #                  "Email help@example.org for more information",
 #                   "terry@example.org", "@carmen", "what@what", "hi@example.org"]
-#[possibility 
-# for possibility in possible_emails 
+#[possibility
+# for possibility in possible_emails
 # if re.search("\A\w+@\w+\.\w{2,3}\Z", possibility)]
